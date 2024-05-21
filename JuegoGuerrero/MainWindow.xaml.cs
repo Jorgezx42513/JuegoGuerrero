02b43fd487eccd5b5f21804e22bd777012fcf5b9
Jorgezx42513 <Jorgezx42513>
2024-05-20 23:14:03 -0700

    first commit

---
+++ JuegoGuerrero/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuegoGuerrero
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnArmor_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnGun_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btnItems_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
